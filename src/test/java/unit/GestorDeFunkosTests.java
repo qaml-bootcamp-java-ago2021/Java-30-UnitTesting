9420f73a59d1517871014b09669be480592156f8
Marco De Jesus <marco_dejesus@live.com>
2021-03-27 03:17:53 -0600

    Creando un caso de prueba que evalua si el Funko que se agrega es el mismo que se lee.

--- src/test/java/unit/GestorDeFunkosTests.java
+++ src/test/java/unit/GestorDeFunkosTests.java
@@ -2,6 +2,7 @@ package unit;
 
 import com.funkos.Funko;
 import com.funkos.GestorDeFunkos;
+import exceptions.FunkoNoEncontradoException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,6 +22,24 @@ public class GestorDeFunkosTests {
 
         // Assert - Afirmar
         Assert.assertTrue(resultadoActual);
+    }
+
+    @Test
+    public void GivenUnaColeccionConUnFunko_WhenLeemosUnFunko_ThenElMismoFunkoEsRetornado() throws FunkoNoEncontradoException {
+        // Arrange - Preparar
+        GestorDeFunkos gestorDeFunkos = new GestorDeFunkos();
+
+        Funko funkoEsperado = new Funko();
+        funkoEsperado.setId(1);
+        funkoEsperado.setNombre("Pacman");
+
+        gestorDeFunkos.agregarFunko(funkoEsperado);
+
+        // Act - Actuar
+        Funko funkoActual = gestorDeFunkos.leerFunkoPorId(funkoEsperado.getId());
+
+        // Assert - Afirmar
+        Assert.assertEquals(funkoActual, funkoEsperado, "Los Funkos no son los mismos.");
     }
 
 }
